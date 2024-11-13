ed765b62d26809faff79c8c363af496042dc972a
Samuel Lemos <samueltdelemos@gmail.com>
2024-11-13 18:20:18 -0300

    feat: implementando o ValidationPipe

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const errorMessages = errors.map(
+          (err) => `${err.property} - ${Object.values(err.constraints).join(', ')}`
+        ).join('; ');
+
+        // Lança uma BadRequestException para capturar o erro de validação
+        return new BadRequestException(`Validation failed: ${errorMessages}`);
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
