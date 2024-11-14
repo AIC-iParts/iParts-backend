517e1888afc049e4405e2158bc7e70f1e1c33a82
Samuel Lemos <samueltdelemos@gmail.com>
2024-11-14 09:17:28 -0300

    chore: removendo app. controller e app.service

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 
 import { ShopModule } from './shop/shop.module';
 
 @Module({
   imports: [ShopModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
