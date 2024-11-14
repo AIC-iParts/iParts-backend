42f4e40076ecb705a40a0032ae05af21d850c1c1
Samuel Lemos <samueltdelemos@gmail.com>
2024-11-14 09:43:51 -0300

    feat: adicionando mais validações no dto do shop

--- app/src/shop/dto/create_shop.dto.ts
+++ app/src/shop/dto/create_shop.dto.ts
@@ -1,4 +1,4 @@
-import { IsDate, IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
+import { IsDate, IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateShopDto {
@@ -6,17 +6,21 @@ export class CreateShopDto {
   name: string;
 
   @IsNotEmpty({ message: 'O CNPJ é obrigatório.' })
+  @IsString()
   cnpj: string;
   
   @IsNotEmpty({ message: 'A senha é obrigatória.' })
+  @IsString()
   @MinLength(6, { message: 'A senha deve ter pelo menos 6 caracteres.' })
   password: string;
 
   @IsNotEmpty({ message: 'O e-mail é obrigatório.' })
+  @IsString()
   @IsEmail({}, { message: 'O e-mail deve ser válido.' })
   email: string;
 
   @IsNotEmpty({ message: 'O número de telefone é obrigatório.' })
+  @IsString()
   phone: string;
 
   @IsNotEmpty({message: 'A data de fundação é obrigatória.'})
@@ -25,29 +29,38 @@ export class CreateShopDto {
   fundation_date: Date;
 
   @IsNotEmpty({ message: 'A rua é obrigatória.' })
+  @IsString()
   street: string;
 
   @IsNotEmpty({ message: 'O número é obrigatório.' })
+  @IsNumber()
   number: number;
 
   @IsNotEmpty({ message: 'A cidade é obrigatória.' })
+  @IsString()
   city: string;
 
   @IsNotEmpty({ message: 'O estado é obrigatório.' })
+  @IsString()
   state: string;
 
   @IsNotEmpty({ message: 'O país é obrigatório.' })
+  @IsString()
   country: string;
 
   @IsNotEmpty({ message: 'O CEP é obrigatório.' })
+  @IsString()
   cep: string;
 
   @IsOptional()
+  @IsString()
   complement: string
 
   @IsOptional()
+  @IsNumber()
   lat: number;
 
   @IsOptional()
+  @IsNumber()
   long: number;
 }
--- app/src/shop/shop.controller.ts
+++ app/src/shop/shop.controller.ts
@@ -5,30 +5,30 @@ import { UpdateShopDto } from './dto/update_shop.dto';
 
 @Controller('shop')
 export class ShopController {
-  constructor(private readonly shopService: ShopService) {}  // Corrigido para shopService
+  constructor(private readonly shopService: ShopService) {}
 
   @Post()
   async create(@Body() createShopDto: CreateShopDto) {
-    return await this.shopService.create(createShopDto);  // Alterado para shopService
+    return await this.shopService.create(createShopDto);
   }
 
   @Get()
   async findAll() {
-    return await this.shopService.findAll();  // Alterado para shopService
+    return await this.shopService.findAll();
   }
 
   @Get(':id')
   async findOne(@Param('id') id: number) {
-    return await this.shopService.findOne(id);  // Alterado para shopService
+    return await this.shopService.findOne(id);
   }
 
   @Patch(':id')
   async update(@Param('id') id: number, @Body() updateShopDto: UpdateShopDto) {
-    return await this.shopService.update(id, updateShopDto);  // Alterado para shopService
+    return await this.shopService.update(id, updateShopDto);
   }
 
   @Delete(':id')
   async remove(@Param('id') id: number) {
-    return await this.shopService.remove(id);  // Alterado para shopService
+    return await this.shopService.remove(id);
   }
 }
