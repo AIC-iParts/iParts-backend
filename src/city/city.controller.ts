a05dde44040b64332dbc7eaa5f34ccea909b2fb2
Samuel Lemos <samueltdelemos@gmail.com>
2024-12-03 18:37:39 -0300

    feat: criando services e controlers para get das cities

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CityService } from './city.service';
 
 @Controller('city')
-export class CityController {}
+export class CityController {
+    constructor(private readonly cityService: CityService) {}
+
+    @Get('/:id_state')
+    async getAllCitiesByStateId(@Param('id_state') id_state : number) {
+        return this.cityService.getAllCitiesByStateId(id_state);
+    }
+}
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class CityService {}
+export class CityService {
+    constructor(private prisma: PrismaService,
+    ) {}
+
+    async getAllCities() { //Retorna todas as cidades
+        return await this.prisma.city.findMany();
+    }
+
+    async getCityById(id_city : number) { //Retorna a cidade do id especificado
+        const city = await this.prisma.city.findUnique({ 
+            where:  {
+               id_city : id_city 
+              } 
+          });
+    
+        if (!city) {
+          throw new NotFoundException(`City with ID ${id_city} not found`);
+        }
+        return city;
+    }
+
+    async getAllCitiesByStateId(id_state : number) { //Retorna todas as cidades que são do state com id fornecido
+        const state = await this.prisma.state.findUnique({ 
+            where:  {
+               id_state : id_state
+              } 
+          });
+    
+        if (!state) {
+          throw new NotFoundException(`State with ID ${id_state} not found`);
+        }
+        
+        return this.prisma.city.findMany({
+            where: {
+                id_state : id_state
+            }
+        });
+    }
+
+}
