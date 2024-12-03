18c188aa2aafcbf6b9de2362bb9bc1276cac3d44
Samuel Lemos <samueltdelemos@gmail.com>
2024-12-03 12:46:48 -0300

    feat: criando modules para country, state, city e address

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 
 import { ShopModule } from './shop/shop.module';
+import { CountryModule } from './country/country.module';
+import { StateModule } from './state/state.module';
+import { CityModule } from './city/city.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
-  imports: [ShopModule],
+  imports: [ShopModule, CountryModule, StateModule, CityModule, AddressModule],
   controllers: [],
   providers: [PrismaService],
 })
