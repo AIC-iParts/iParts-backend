310513ac2db259c50dfb8618ffd26ac0dbfeec0e
Samuel Lemos <samueltdelemos@gmail.com>
2024-12-03 17:54:00 -0300

    feat: adicionando controllers para addrerss, city, state e country

--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { GeocodingService } from 'src/geocoding/geocoding.service';
+import { AddressController } from './address.controller';
 
 @Module({
-  providers: [AddressService]
+  controllers: [AddressController],
+  providers: [AddressService, PrismaService, GeocodingService]
 })
 export class AddressModule {}
