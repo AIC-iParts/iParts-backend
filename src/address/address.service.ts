b50a70cb18557db265dda3e5db206d0a35c84f78
Samuel Lemos <samueltdelemos@gmail.com>
2024-12-03 21:19:42 -0300

    feat: criando get de all addresses e get por id

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpException, HttpStatus, ConflictException } from '@nestjs/common';
+import { GeocodingService } from 'src/geocoding/geocoding.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateAddressDto } from './dto/create_address.dto';
 
 @Injectable()
-export class AddressService {}
+export class AddressService {
+    constructor(private prisma: PrismaService,
+        private readonly geocodingService: GeocodingService
+      ) {}
+
+    async getAllAddresses() {
+        return await this.prisma.address.findMany();
+    }
+
+    async getAddressById(id_address : number) {
+        const address = await this.prisma.address.findUnique({ 
+            where:  {
+               id_address : id_address 
+              } 
+          });
+    
+        if (!address) {
+          throw new NotFoundException(`Address with ID ${id_address} not found`);
+        }
+        return address;
+    }
+
+    async createAddress(createAddressDto : CreateAddressDto) {
+
+    }
+}
