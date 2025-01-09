ba634b2e4b815711c0e6909e9e818114ecdc18b8
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-09 16:18:40 -0300

    feat: criando service de getClientById

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { GeocodingService } from 'src/geocoding/geocoding.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ResponseClientDto } from './dto/response_client.dto';
 
 @Injectable()
 export class ClientService {
-    constructor(private prisma: PrismaService,
-        private readonly geocodingService: GeocodingService
-      ) {}
+  constructor(private prisma: PrismaService,
+      private readonly geocodingService: GeocodingService
+    ) {}
+  
+  async getClientById(id_client: number) {
+    const client = await this.prisma.client.findUnique({
+      where: {
+        id_client: id_client,
+      },
+    });
 
+    if(!client) {
+      throw new NotFoundException(`Client with ID ${id_client} not found`);
+    }
+
+    return plainToInstance(ResponseClientDto, client, { excludeExtraneousValues: true });
+  }
 }
