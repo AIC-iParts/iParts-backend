cc48383550ca3545417ff9c8d1d03ecb896871e6
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-09 15:04:35 -0300

    feat: criando service para cadastro de endereço

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -2,31 +2,52 @@ import { Injectable, NotFoundException, HttpException, HttpStatus, ConflictExcep
 import { GeocodingService } from 'src/geocoding/geocoding.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateAddressDto } from './dto/create_address.dto';
+import { Prisma } from '@prisma/client';
+import { plainToInstance } from 'class-transformer';
+import { ResponseAddressDto } from './dto/response_address.dto';
 
 @Injectable()
 export class AddressService {
-    constructor(private prisma: PrismaService,
-        private readonly geocodingService: GeocodingService
-      ) {}
+  constructor(private prisma: PrismaService,
+      private readonly geocodingService: GeocodingService
+    ) {}
 
-    async getAllAddresses() {
-        return await this.prisma.address.findMany();
+  async getAllAddresses() {
+      return await this.prisma.address.findMany();
+  }
+
+  async getAddressById(id_address : number) {
+      const address = await this.prisma.address.findUnique({ 
+          where:  {
+              id_address : id_address 
+            } 
+        });
+  
+      if (!address) {
+        throw new NotFoundException(`Address with ID ${id_address} not found`);
+      }
+      return address;
+  }
+
+  async createAddress(createAddressDto : CreateAddressDto){
+    try {
+      const newAddress = await this.prisma.address.create({
+        data: {
+          ...createAddressDto,
+        } as Prisma.AddressUncheckedCreateInput,
+      });
+  
+      return {
+        message: "Endereço cadastrado com sucesso!",
+        address: plainToInstance(ResponseAddressDto, newAddress, {excludeExtraneousValues: true}),
+      };
+
+    } catch (error) {
+      //console.log(error)
+      throw new HttpException(
+        error.message,
+        error.status,
+      );
     }
-
-    async getAddressById(id_address : number) {
-        const address = await this.prisma.address.findUnique({ 
-            where:  {
-               id_address : id_address 
-              } 
-          });
-    
-        if (!address) {
-          throw new NotFoundException(`Address with ID ${id_address} not found`);
-        }
-        return address;
-    }
-
-    async createAddress(createAddressDto : CreateAddressDto) {
-
-    }
+  }
 }
--- src/address/dto/create_address.dto.ts
+++ src/address/dto/create_address.dto.ts
@@ -4,6 +4,11 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateAddressDto {
     @ApiProperty()
     @IsNotEmpty()
+    @IsNumber()
+    id_client: number;
+
+    @ApiProperty()
+    @IsNotEmpty()
     @IsString()
     name: string;
     
