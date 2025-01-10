af0a6e0dacf1aa4fe4d619b2b42b2b3264b16fd0
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-10 20:09:02 -0300

    fix: adicionando campos lat e long no createAddress service que estava dando erro

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -2,7 +2,6 @@ import { Injectable, NotFoundException, HttpException, HttpStatus, ConflictExcep
 import { GeocodingService } from 'src/geocoding/geocoding.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateAddressDto } from './dto/create_address.dto';
-import { Prisma } from '@prisma/client';
 import { plainToInstance } from 'class-transformer';
 import { ResponseAddressDto } from './dto/response_address.dto';
 import { ClientService } from 'src/client/client.service';
@@ -38,10 +37,17 @@ export class AddressService {
       await this.clientService.getClientById(createAddressDto.id_client); // verifica se o client existe
       await this.cityService.getCityById(createAddressDto.id_city); // verifica se a cidade informada é válida
 
+      //obtendo lat e long
+      const coordinates = await this.geocodingService.getCoordinates(
+        `${createAddressDto.cep}`
+      );
+
       const newAddress = await this.prisma.address.create({
         data: {
           ...createAddressDto,
-        } as Prisma.AddressUncheckedCreateInput,
+          lat: coordinates.lat,
+          long: coordinates.long
+        },
       });
   
       return {
