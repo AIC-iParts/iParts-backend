fffe13cc53e4a575f9b170373659d6b257b5f139
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-10 20:09:53 -0300

    feat: criando createClient service e criando controllers de create e get por id

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { ClientService } from './client.service';
+import { CreateClientDto } from './dto/create_client.dto';
 
 @Controller('client')
-export class ClientController {}
+export class ClientController {
+    constructor(private readonly clientService: ClientService) {}
+
+    @Post()
+    async createClientController(@Body() createClientDto: CreateClientDto) {
+    return await this.clientService.createClient(createClientDto);
+    }
+
+    @Get(':id_client')
+    async getClientByIdController(@Param('id_client') id_client: number) {
+    return await this.clientService.getClientById(id_client);
+    } 
+}
--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,15 +1,68 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
-import { GeocodingService } from 'src/geocoding/geocoding.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ResponseClientDto } from './dto/response_client.dto';
+import { CreateClientDto } from './dto/create_client.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class ClientService {
   constructor(private prisma: PrismaService,
-      private readonly geocodingService: GeocodingService
     ) {}
   
+  async createClient(createClientDto: CreateClientDto) {
+    try {
+      const {confirmPassword, ...rest} = createClientDto
+      // Verifica se já existe um cliente com o mesmo CPF ou E-mail
+      const existingClient = await this.prisma.client.findFirst({
+        where: {
+          OR: [
+            { cpf: createClientDto.cpf },
+            { email: createClientDto.email },
+          ],
+        },
+      });
+
+      // Se a loja já existir, lança uma exceção de conflito
+      if (existingClient) {
+        if (existingClient.cpf === createClientDto.cpf) {
+          throw new ConflictException('CPF já registrado.');
+        }
+        if (existingClient.email === createClientDto.email) {
+          throw new ConflictException('E-mail já registrado.');
+        }
+      }
+
+      if (createClientDto.password != confirmPassword) {
+        throw new ConflictException('As senhas não coincidem.')
+      }
+
+      //gerando hash da senha
+      const saltOrRounds = 10;
+      const hashedPassword = await bcrypt.hash(createClientDto.password, saltOrRounds);
+
+      const newClient = await this.prisma.client.create({
+        data: {
+          ...rest,
+          password: hashedPassword,
+        },
+      });
+
+      return {
+        message: "Cliente cadastrado com sucesso!",
+        statusCode: 201,
+        client: plainToInstance(ResponseClientDto, newClient, { excludeExtraneousValues: true }),
+      };
+
+    } catch (error) {
+      //console.log(error)
+      throw new HttpException(
+        error.status,
+        error.message
+      );
+    }    
+  }
+
   async getClientById(id_client: number) {
     const client = await this.prisma.client.findUnique({
       where: {
