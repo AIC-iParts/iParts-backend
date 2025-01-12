da8716e637b02dd92d0217399167d940eff791cf
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-12 16:46:40 -0300

    feat: criando alguns services para manipulação do country

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -1,18 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ResponseCountryDto } from './dto/response_country.dto';
 
 @Injectable()
 export class CountryService {
+    constructor(
+        private prisma: PrismaService,
+    ) {}        
 
     async getAllCountries() {
-
+        const countries = await this.prisma.country.findMany();
+        return plainToInstance(ResponseCountryDto, countries, {excludeExtraneousValues: true});
     }
 
-    async getCountryById() {
-
+    async getCountryById(id_country: number) {
+        const country = await this.prisma.country.findUnique({ 
+                    where:  {
+                       id_country : id_country
+                      } 
+                  });
+            
+                if (!country) {
+                  throw new NotFoundException(`Country with ID ${id_country} not found`);
+                }
+                return plainToInstance(ResponseCountryDto, country, {excludeExtraneousValues: true});
     }
 
-    async getCountryByName() {
-        
+    async getCountryByCountryCode(country_code: string) {
+      const country = await this.prisma.country.findFirst({ 
+        where:  {
+           country_code : country_code
+          }
+      });
+
+      if (!country) {
+        throw new NotFoundException(`Country with code ${country_code} not found`);
+      }
+      return plainToInstance(ResponseCountryDto, country, {excludeExtraneousValues: true});
     }
     
 }
