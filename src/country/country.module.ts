45fb430aefa0aaaa3644bc85707bc6ce2c62e84b
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-12 17:09:51 -0300

    fix: importando PrismaService no country module

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CountryService } from './country.service';
 import { CountryController } from './country.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  providers: [CountryService],
+  providers: [CountryService, PrismaService],
   controllers: [CountryController]
 })
 export class CountryModule {}
