24fdc9fd88d027d5e36966945e5122956da37b4d
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-14 12:22:15 -0300

    refactor: simplificando a maneira que os dados são pegados da .env no auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,22 +3,17 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ShopModule } from 'src/shop/shop.module';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
   imports: [
     ShopModule,
-    ConfigModule.forRoot(),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1h' },
+    JwtModule.register({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
       }),
-    }),
-  ],
-})
+    ]
+  })
+
 export class AuthModule {}
