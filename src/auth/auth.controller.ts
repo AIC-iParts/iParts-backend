6b8058e71248d036a748eda54aa79df74a2ac7c9
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-16 12:47:50 -0300

    feat: adicionando controller de login do client

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 import { LoginShopDto } from './dto/login_shop.dto';
 import { AuthService } from './auth.service';
+import { LoginClientDto } from './dto/login_client.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +12,11 @@ export class AuthController {
     @ApiOperation({summary: 'Login da loja.'})
     async loginShopController(@Body() loginShopDto: LoginShopDto) {
     return await this.authService.loginShop(loginShopDto);
+    }
+
+    @Post('/loginClient')
+    @ApiOperation({summary: 'Login do cliente.'})
+    async loginClientController(@Body() loginClientDto: LoginClientDto) {
+    return await this.authService.loginClient(loginClientDto);
     }
 }
