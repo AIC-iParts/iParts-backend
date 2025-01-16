9f9c5a47745afd1fc0231c9ec091c462aee3afae
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-16 11:40:31 -0300

    fix: adicionando importação do client module no auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ShopModule } from 'src/shop/shop.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ClientModule } from 'src/client/client.module';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
   imports: [
     ShopModule,
+    ClientModule,
     JwtModule.register({
         secret: process.env.JWT_SECRET,
         signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
