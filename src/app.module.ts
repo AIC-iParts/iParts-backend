147ec849fb87180c7aafe0697990483087259d27
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-16 11:32:44 -0300

    feat: criando guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,19 @@ import { AddressModule } from './address/address.module';
 import { CacheModule } from './cache/cache.module';
 import { ClientModule } from './client/client.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './auth/roles.guard';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ShopModule, CountryModule, StateModule, CityModule, AddressModule, CacheModule, ClientModule, AuthModule],
+  imports: [ShopModule, CountryModule, StateModule, CityModule, AddressModule, CacheModule, ClientModule, AuthModule, JwtModule],
   controllers: [],
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard
+    }
+  ],
 })
 export class AppModule {}
---
+++ src/auth/user_type.enum.ts
@@ -0,0 +1,4 @@
+export enum UserType {
+    Shop = 'shop',
+    Client = 'client',
+}
