2fd8c199de2cb853acb28c8d6e14b99983f058a7
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-17 20:31:10 -0300

    feat: criando dto para cadastro de pedido

---
+++ src/order/dto/create_order.dto.ts
@@ -0,0 +1,38 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsArray, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength, ValidateNested } from "class-validator";
+
+export class CreateOrderDto { 
+    @ApiProperty()
+    @IsOptional()
+    @IsString()
+    observations?: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    id_shop: number;
+    
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    id_client: number;
+
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => CreateOrderProductDto)
+    order_products: CreateOrderProductDto[];
+}
+
+class CreateOrderProductDto {
+    @IsNumber()
+    id_product: number;
+  
+    @IsNumber()
+    @MinLength(1)
+    amount: number;
+  
+    @IsNumber()
+    @MinLength(0)
+    price: number;
+  }
