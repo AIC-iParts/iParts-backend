19f7f8123ec3162949e1fb12358fa9410d65229c
Samuel Lemos <samueltdelemos@gmail.com>
2025-01-17 14:57:24 -0300

    feat: criando primeiros services e controllers para products (cadastro não está funcionando e por isso está comentado o código)

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { Roles } from 'src/auth/roles.decorator';
+import { UserType } from 'src/auth/user_type.enum';
+import { CreateProductDto } from './dto/create_product.dto';
 
+@ApiBearerAuth()
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    constructor(private readonly productService: ProductService) {}
+
+    @Post()
+    @Roles(UserType.Shop)
+    @ApiOperation({summary: 'Cadastra um novo produto à loja.'})
+    async createProductController(@Req() request: Request, @Body() createProductDto: CreateProductDto) {
+        return await this.productService.createProduct(createProductDto, request);
+    }
+
+    @Get(':id_product')
+    @ApiOperation({summary: 'Retorna o produto selecionado.'})
+    async getProductByIdController(@Param('id_product') id_product: number) {
+        return await this.productService.getProductById(id_product);
+    }
+
+    @Get(':id_shop')
+    @ApiOperation({summary: 'Retorna todos os produtos da loja selecionado.'})
+    async getAllProductsForShopController(@Param('id_product') id_shop: number) {
+        return await this.productService.getAllProductsForShop(id_shop);
+    }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProductDto } from './dto/create_product.dto';
+import { ShopService } from 'src/shop/shop.service';
+import { plainToInstance } from 'class-transformer';
+import { ResponseProductDto } from './dto/response_product.dto';
+import { UpdateProductDto } from './dto/update_product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(
+    private prisma: PrismaService,
+    private readonly shopService: ShopService
+    ) {}
+
+    async createProduct(createProductDto: CreateProductDto, request: Request) {
+        const shop = request['user']
+        try {
+            await this.shopService.getShopByIdService(shop.id)
+            console.log(shop.id)
+            /*
+            const newProduct = await this.prisma.product.create({
+            data: {
+                ...createProductDto,
+                id_shop: shop.id,
+                },
+            });
+
+            return {
+                message: "Produto cadastrado com sucesso!",
+                statusCode: 201,
+                product: plainToInstance(ResponseProductDto, newProduct, { excludeExtraneousValues: true }),
+            };
+
+            */
+        } catch (error) {
+            //console.log(error)
+            throw new HttpException(
+            error.message,
+            error.status,
+            );
+        }
+    }
+    
+
+    async getAllProductsForShop(id_shop: number) {
+        const products = await this.prisma.product.findMany({
+            where: {id_shop: id_shop}
+        });
+
+        if (!products) {
+            throw new NotFoundException(`Products for Shop ID ${id_shop} not found`);
+        }
+
+        return plainToInstance(ResponseProductDto, products, { excludeExtraneousValues: true });
+    }
+
+    async getProductById(id_product: number) {
+        const product = await this.prisma.product.findUnique({
+            where: {id: id_product}
+        })
+
+        if (!product) {
+            throw new NotFoundException(`Product with ID ${id_product} not found`);
+        }
+
+        return plainToInstance(ResponseProductDto, product, {excludeExtraneousValues: true})
+    }
+}
