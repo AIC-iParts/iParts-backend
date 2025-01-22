ac0780005ec940b428da460e4203550da1ee154b
Samuel <samueltdelemos@gmail.com>
2025-01-22 08:59:21 -0300

    feat: adicionando controllers de client

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { CreateClientDto } from './dto/create_client.dto';
 import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { Public } from 'src/auth/auth.decorator';
+import { Roles } from 'src/auth/roles.decorator';
+import { UserType } from 'src/auth/user_type.enum';
+import { UpdateClientDto } from './dto/update_client.dto';
 
 @Controller('client')
 export class ClientController {
@@ -20,5 +23,21 @@ export class ClientController {
     @ApiOperation({summary: 'Retorna o cliente selecionado.'})
     async getClientByIdController(@Param('id_client') id_client: number) {
     return await this.clientService.getClientById(id_client);
-    } 
+    }
+
+    @Patch()
+    @Roles(UserType.Client)
+    @ApiBearerAuth()
+    @ApiOperation({summary: 'Atualiza o cliente selecionado.'})
+    async updateClientController(@Req() request: Request, @Body() updateClientDto: UpdateClientDto) {
+        return
+    }
+
+    @Delete()
+    @Roles(UserType.Client)
+    @ApiBearerAuth()
+    @ApiOperation({summary: 'Deleta o cliente selecionado.'})
+    async deleteClientController(@Req() request: Request) {
+      return
+    }
 }
